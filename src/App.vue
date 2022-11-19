90a64729a372acb42192c5606bb318d8c4940f06
jinan <jinan5694@163.com>
2022-11-19 11:26:16 +0800

    复现问题

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+import TheTest from './components/TheTest.vue'
+const value = ref('')
+const filters = ref([])
 </script>
 
 <template>
   <div id="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <p>输入些内容，会触发 TheTest 组件对 filters 属性的 watch</p>
+    <p>input some thing, will emit TheTest component watch of props filter</p>
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
-
-    <main>
-      <TheWelcome />
-    </main>
+    <input type="text" v-model="value" placeholder="input here">
+    <TheTest :filters="[{ foo: 'bar' }]"></TheTest>
   </div>
 </template>
 
---
+++ src/components/TheTest.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    filters: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      logs: []
+    }
+  },
+  watch: {
+    filters: function () {
+      console.log('watch filters');
+      this.logs.push('watch filters emited.')
+    }
+  }
+}
+
+</script>
+
+<template>
+  <pre>{{ logs }}</pre>
+</template>
