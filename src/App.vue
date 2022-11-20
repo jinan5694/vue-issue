85787e607c5a086892fee937de49bd83002fe496
jinan <jinan5694@163.com>
2022-11-21 05:49:29 +0800

    add comment

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
 import { ref } from 'vue';
 import TheTest from './components/TheTest.vue'
+
+// the value is not relative any thing
 const value = ref('')
-const filters = ref([])
 </script>
 
 <template>
   <div id="app">
-    <p>输入些内容，会触发 TheTest 组件对 filters 属性的 watch</p>
+    <h1>Case</h1>
     <p>input some thing, will emit TheTest component watch of props filter</p>
-
+    <p>输入些内容，会触发 TheTest 组件对 filters 属性的 watch</p>
     <input type="text" v-model="value" placeholder="input here">
-    <TheTest :filters="[{ foo: 'bar' }]"></TheTest>
+    <!-- the value of filters is a "normal data" -->
+    <TheTest :filters="[{ foo: 'bar'}]"></TheTest>
   </div>
 </template>
 
@@ -25,21 +27,5 @@ header {
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
